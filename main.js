ffa1ad0a81e4431ede90b5927ca5a775d5ee643e
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-03-04 15:54:48 -0600

    Add basic client-side form validation
    I chose to use JavaScript for the client-side validation because HTML
    validation doesn't have the methods and states I would like to manipulate

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 const root = document.documentElement;
 
+// Light and Dark mode
 root.className = "light";
+
+// Client-side form Validation
+const inputs = document.querySelectorAll("input");
+
+
+inputs.forEach(input => {
+    input.addEventListener("change", e => {
+        e.target.dataset.valuestate = validateTextInput(e.target.value);
+
+        console.log(`${e.target.value} ${e.target.dataset.valuestate}`);
+    });
+});
+
+function validateTextInput(value) {
+    if (value.length >= 2 && value.length <= 50) {
+        return "valid";
+    }
+    else if (value.length == 0) {
+        return "empty";
+    }
+    else {
+        return "invalid";
+    }
+}
