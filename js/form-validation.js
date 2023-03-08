efbc030d2524f7c5d47a2ddda4b9a4524f063880
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-03-08 17:38:10 -0600

    add generic validation to inputs
    The method validateGenericInput can show errors when the inputs
    are empty

--- js/form-validation.js
+++ js/form-validation.js
@@ -6,9 +6,39 @@ const phoneNumberInput = document.querySelector("#phone");
 const passwordInput = document.querySelector("#password1");
 const confirmPasswordInput = document.querySelector("#password2");
 
+const inputs = document.querySelectorAll("input");
+
 // Select form
 const form = document.querySelector("#main-form");
 
+// Functions
+function displayError(inputElement, errorMessage) {
+    setValueState(inputElement, "invalid");
+
+    const validationSpan = inputElement.nextElementSibling.nextElementSibling;
+    validationSpan.innerText = "* " + errorMessage;
+};
+
+function setValueState(inputElement, newValue) {
+    inputElement.dataset.valuestate = newValue;
+}
+
+// Validations
+function validateGenericInput(inputElement) {
+    if (inputElement.value.length == 0) {
+        displayError(inputElement, "Field cannot be empty");
+    } else {
+        setValueState(inputElement, "valid");
+    }
+}
+
+//Events
 form.addEventListener("submit", e => {
     e.preventDefault();
-})
+});
+
+inputs.forEach(input => {
+    input.addEventListener("change", e => {
+        validateGenericInput(e.target);
+    });
+});
