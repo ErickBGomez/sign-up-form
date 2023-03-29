207b1cba58eb9fbe996c9b38b4064a458f2ca65d
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-03-29 11:36:45 -0600

    Change theme icon depending on currentTheme

--- js/change-theme.js
+++ js/change-theme.js
@@ -1,8 +1,23 @@
+let currentTheme = "light";
+
 const root = document.documentElement;
 const toggleThemeElement = document.querySelector(".theme-container");
+const themeIcon = toggleThemeElement.querySelector(".theme-icon");
+const themeText = toggleThemeElement.querySelector(".theme-text");
 
 function toggleTheme() {
-    root.className = (root.className == "light") ? "dark" : "light";
+    currentTheme = (currentTheme == "light") ? "dark" : "light";
+    setTheme(currentTheme);
+}
+
+function setTheme(newTheme) {
+    root.className = newTheme;
+    themeIcon.innerText = `${newTheme}_mode`;
+    themeText.innerText = `${toCapitalize(newTheme)} theme`;
+}
+
+function toCapitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 toggleThemeElement.addEventListener("click", toggleTheme);
