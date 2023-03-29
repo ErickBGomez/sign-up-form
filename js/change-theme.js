49ffc594730c4dcfad4260cf5cb16c42cdf61cff
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-03-29 11:19:00 -0600

    Add toggleTheme funcionality-

--- js/change-theme.js
+++ js/change-theme.js
@@ -1,4 +1,11 @@
-let currentTheme = "light";
-
 const root = document.documentElement;
+const toggleThemeElement = document.querySelector(".theme-container");
+
+function toggleTheme() {
+    root.className = (root.className == "light") ? "dark" : "light";
+}
+
+toggleThemeElement.addEventListener("click", toggleTheme);
+
+// Initial theme: Light
 root.className = "light";
