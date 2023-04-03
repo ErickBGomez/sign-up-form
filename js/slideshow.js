f664ac8c39f996a53a18fa37494f03985dfcd0b7
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-04-03 16:22:45 -0600

    Add basic slideshow functions

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,1 +1,21 @@
-let currentSlideshowValue = 1;
+currentIndex = 0;
+const slideshowImages = document.querySelectorAll(".slideshow-content");
+
+function setSlideshowContent(index) {
+    currentIndex = index;
+
+    slideshowImages.forEach(image => {
+        image.dataset.visibilitystate = "hide";
+    });
+
+    slideshowImages[currentIndex].dataset.visibilitystate = "show";
+}
+
+function moveSlideshowContent(numDirection = 1) {
+    currentIndex += numDirection;
+
+    if (currentIndex < 0) currentIndex = slideshowImages.length - 1;
+    else if (currentIndex > slideshowImages.length - 1) currentIndex = 0;
+
+    setSlideshowContent(currentIndex);
+}
